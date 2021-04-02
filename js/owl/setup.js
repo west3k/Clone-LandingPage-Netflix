625678d18bdab8cc65883fe48798e84f981ad5a4
Wesley Oliveira Silva <58710241+west3k@users.noreply.github.com>
2021-04-02 12:26:52 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,129 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+function changeToBlackClover(){
+
+    let capa = document.getElementsByClassName('filme-principal')[0]
+    let titulo = document.getElementsByClassName('titulo')[0]
+    let descricao = document.getElementsByClassName('descricao')[0]
+    capa.style.background = 'linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url(../img/capas/capa-bc.jpg)'
+    capa.style.backgroundSize = 'cover'
+    titulo.innerHTML = 'BLACK CLOVER'
+    descricao.innerHTML = 'Num mundo onde magia é tudo, Asta e Yuno são abandonados em uma igreja no mesmo dia. Enquanto Yuno possui poderes mágicos excepcionais, Asta é a única pessoa do mundo todo desprovida desse dom.'
+}
+function changeToBadBoys(){
+
+    let capa = document.getElementsByClassName('filme-principal')[0]
+    let titulo = document.getElementsByClassName('titulo')[0]
+    let descricao = document.getElementsByClassName('descricao')[0]
+    capa.style.background = 'linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url(../img/capas/capa-bb.jpg)'
+    capa.style.backgroundSize = 'cover'
+    titulo.innerHTML = 'BAD BOYS'
+    descricao.innerHTML = 'Armando é um assassino de sangue frio com uma natureza cruel e provocadora. Ele está comprometido com o trabalho do cartel e é enviado por sua mãe para matar Mike (Smith). Nuñez assumirá o papel de Rita, psicóloga criminal durona e engraçada que é recém-nomeada chefe da AMMO e é ex-namorada de Mike.'
+}
+function changeToRickMorty(){
+
+    let capa = document.getElementsByClassName('filme-principal')[0]
+    let titulo = document.getElementsByClassName('titulo')[0]
+    let descricao = document.getElementsByClassName('descricao')[0]
+    capa.style.background = 'linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url(../img/capas/capa-rm.jpg)'
+    capa.style.backgroundSize = 'cover'
+    titulo.innerHTML = 'RICK AND MORTY'
+    descricao.innerHTML = 'Rick é um velho mentalmente desequilibrado, mas cientificamente talentoso que se reconectou recentemente com sua família. Ele passa a maior parte do tempo envolvendo o jovem neto Morty em aventuras perigosas e estranhas no espaço e em universos alternativos. Compostos à vida familiar já instável de Morty, esses eventos causam muito sofrimento a Morty em casa e na escola.'
+}
+function changeToBurrasNada(){
+
+    let capa = document.getElementsByClassName('filme-principal')[0]
+    let titulo = document.getElementsByClassName('titulo')[0]
+    let descricao = document.getElementsByClassName('descricao')[0]
+    capa.style.background = 'linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url(../img/capas/capa-dbn.jpg)'
+    capa.style.backgroundSize = 'cover'
+    titulo.innerHTML = 'DE BURRAS NADA'
+    descricao.innerHTML = 'Após ser traída pelo namorado, Cristina procura um colega de quarto e encontra Alejandro, a quem aceita em sua vida porque acredita que ele é gay. Quando Alejandro decide confessar a verdade, é tarde demais, ambos estão completamente apaixonados.'
+}
+function changeToWandaVision(){
+
+    let capa = document.getElementsByClassName('filme-principal')[0]
+    let titulo = document.getElementsByClassName('titulo')[0]
+    let descricao = document.getElementsByClassName('descricao')[0]
+    capa.style.background = 'linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url(../img/capas/capa-wv.jpg)'
+    capa.style.backgroundSize = 'cover'
+    titulo.innerHTML = 'WANDAVISION'
+    descricao.innerHTML = 'Wanda Maximoff e Visão, um casal de super-heróis com uma vida perfeita, que começa a suspeitar que nem tudo é o que parece.'
+}
+function changeToTomJerry(){
+
+    let capa = document.getElementsByClassName('filme-principal')[0]
+    let titulo = document.getElementsByClassName('titulo')[0]
+    let descricao = document.getElementsByClassName('descricao')[0]
+    capa.style.background = 'linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url(../img/capas/capa-tj.jpg)'
+    capa.style.backgroundSize = 'cover'
+    titulo.innerHTML = 'TOM & JERRY'
+    descricao.innerHTML = 'Um gato de rua chamado Tom é contratado por uma garota chamada Kayla, uma jovem empregada que trabalha em um hotel glamouroso em Nova York, para se livrar de Jerry, um rato travesso que se mudou para o hotel, antes que ele seja a ruína de um importante casamento.'
+}
+
+function changeToTenet(){
+
+    let capa = document.getElementsByClassName('filme-principal')[0]
+    let titulo = document.getElementsByClassName('titulo')[0]
+    let descricao = document.getElementsByClassName('descricao')[0]
+    capa.style.background = 'linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url(../img/capas/capa-ten.jpg)'
+    capa.style.backgroundSize = 'cover'
+    titulo.innerHTML = 'TENET'
+    descricao.innerHTML = 'Armado com apenas uma palavra – Tenet – e lutando pela sobrevivência do mundo inteiro, o Protagonista viaja através de um mundo crepuscular de espionagem internacional em uma missão que irá desenrolar em algo para além do tempo real.'
+}
+function changeToSupermanLois(){
+
+    let capa = document.getElementsByClassName('filme-principal')[0]
+    let titulo = document.getElementsByClassName('titulo')[0]
+    let descricao = document.getElementsByClassName('descricao')[0]
+    capa.style.background = 'linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url(../img/capas/capa-sl.jpg)'
+    capa.style.backgroundSize = 'cover'
+    titulo.innerHTML = 'SUPERMAN & LOIS'
+    descricao.innerHTML = 'A vida e a rotina de Superman e Lois Lane como pais e repórteres após a Crise.'
+}
+function changeToShingeki(){
+
+    let capa = document.getElementsByClassName('filme-principal')[0]
+    let titulo = document.getElementsByClassName('titulo')[0]
+    let descricao = document.getElementsByClassName('descricao')[0]
+    capa.style.background = 'linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url(../img/capas/capa-sk.jpg)'
+    capa.style.backgroundSize = 'cover'
+    titulo.innerHTML = 'SHINGEKI NO KYOJIN'
+    descricao.innerHTML = 'Titãs estão quase exterminando a raça humana. Porém alguns estão dispostos a formar um exército de ataque aos seres assassinos. O jovem Eren, após ver sua mãe ser devorada por um titã, decide que não deixará nenhum deles vivo e buscará sua vingança completa.'
+}
+function changeToMulherMaravilha(){
+
+    let capa = document.getElementsByClassName('filme-principal')[0]
+    let titulo = document.getElementsByClassName('titulo')[0]
+    let descricao = document.getElementsByClassName('descricao')[0]
+    capa.style.background = 'linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url(../img/capas/capa-mm.jpg)'
+    capa.style.backgroundSize = 'cover'
+    titulo.innerHTML = 'MULHER MARAVILHA 1984'
+    descricao.innerHTML = 'Em 1984, Diana Prince entra em conflito com dois inimigos formidáveis, Maxwell Lord e a Mulher-Leopardo, enquanto reencontra misteriosamente com seu antigo interesse amoroso Steve Trevor.'
+}
+function changeToJujustu(){
+
+    let capa = document.getElementsByClassName('filme-principal')[0]
+    let titulo = document.getElementsByClassName('titulo')[0]
+    let descricao = document.getElementsByClassName('descricao')[0]
+    capa.style.background = 'linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url(../img/capas/capa-jk.jpg)'
+    capa.style.backgroundSize = 'cover'
+    titulo.innerHTML = 'JUJUSTU KAISEN'
+    descricao.innerHTML = 'Jujutsu Kaisen Yuji é um gênio do atletismo, mas não tem interesse algum em ficar correndo em círculos. Ele é feliz como membro no Clube de Estudo de Fenômenos Psíquicos. Apesar de estar no clube apenas por diversão, tudo fica sério quando um espírito de verdade aparece na escola! A vida está prestes a se tornar muito interessante na Escola Sugisawa…'
+}
